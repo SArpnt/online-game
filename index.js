a836fea354ff8fdd44421d34ab55a009e4780dee
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-03-22 21:27:05 -0400

    dynamic port

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+var port = processes.env.PORT||6980
 // Dependencies
 var express = require('express')
 var http = require('http')
 var path = require('path')
 var socketIO = require('socket.io'); var app = express()
 var server = http.Server(app)
-var io = socketIO(server); app.set('port', 6980)
+var io = socketIO(server); app.set('port', port)
 app.use('/static', express.static(__dirname + '/static'))// Routing
 app.get('/', function (request, response) {
 	response.sendFile(path.join(__dirname, 'index.html'))
 })// Starts the server.
-server.listen(6980, function () {
-	console.log('Starting server on port 6980')
+server.listen(port, function () {
+	console.log('Starting server on port '+port)
 })
 // Add the WebSocket handlers
 io.on('connection', function (socket) {
